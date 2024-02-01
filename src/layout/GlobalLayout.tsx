3f9dc6f1a4bd2f947c051529ac70b51fc53a392c
minkyEazel <minky@eazel.net>
2024-02-02 00:06:14 +0900

    🎨 (layout): Add GlobalLayout for applying all pages
    공통 스타일을 위한 레이아웃을 분리합니다.

---
+++ src/layout/GlobalLayout.tsx
@@ -0,0 +1,11 @@
+import { styled } from 'styled-components'
+
+const Global = styled.div`
+  display: flex;
+  width: 100vw;
+  min-height: 100dvh;
+  background-color: #f0ebf8;
+  justify-content: center;
+`
+
+export const Layout = { Global }

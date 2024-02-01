37bf9f37c19742132c7947c43c4429e891d8a101
minkyEazel <minky@eazel.net>
2024-02-02 00:04:31 +0900

    🎨 (route): Split page with Questionnaire, Preview and Sheet
    작성해야 하는 경로 3곳을 분리합니다.

---
+++ src/components/Questionnaire/index.tsx
@@ -0,0 +1,7 @@
+import { Layout } from '../../layout/GlobalLayout'
+
+function Questionnaire() {
+  return <Layout.Global></Layout.Global>
+}
+
+export default Questionnaire
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { Provider } from 'react-redux'
+import store from './store'
+import GlobalStyles from './style/globalCss'
+import { Route, BrowserRouter, Routes } from 'react-router-dom'
+import Questionnaire from './pages/Questionnaire'
+import Preview from './pages/Preview'
+import Sheet from './pages/Sheet'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Questionnaire />} />
+          <Route path='/preview' element={<Preview />} />
+          <Route path='/sheet' element={<Sheet />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 )

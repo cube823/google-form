dc12b91283a494e37183a906f973cad055dc3e8c
minkyEazel <minky@eazel.net>
2024-02-04 09:53:52 +0900

    🎨 (switch): Add Switch component

---
+++ src/components/Widget/Switch/index.tsx
@@ -0,0 +1,47 @@
+import * as Switch from '@radix-ui/react-switch'
+import { styled } from 'styled-components'
+
+interface QuestionSwitchPros {
+  checked: boolean
+  onCheckedChange: (checked: boolean) => void
+}
+
+const QuestionSwitch = ({ checked, onCheckedChange }: QuestionSwitchPros) => {
+  return (
+    <StyledSwitchRoot checked={checked} onCheckedChange={onCheckedChange}>
+      <StyledSwitchThumb />
+    </StyledSwitchRoot>
+  )
+}
+
+const StyledSwitchRoot = styled(Switch.Root)`
+  width: 42px;
+  height: 25px;
+  background-color: ${(props) => props.theme.colors.black};
+  border-radius: 9999px;
+  position: relative;
+  padding: 0;
+  box-shadow: inset 1px 1px 0 rgba(0, 0, 0, 0.1), inset 0 -1px 0 rgba(0, 0, 0, 0.07);
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+
+  &[date-state='checked'] {
+    background-color: ${(props) => props.theme.colors.purple_dark};
+  }
+`
+
+const StyledSwitchThumb = styled(Switch.Thumb)`
+  display: block;
+  width: 21px;
+  height: 21px;
+  transform: translateX(2px);
+  background-color: white;
+  border-radius: 9999px;
+  transition: transform 100ms;
+  will-change: transform;
+
+  &[data-state='checked'] {
+    transform: translateX(19px);
+  }
+`
+
+export default QuestionSwitch

bce4eed6a750081e3d1b1fb8f09a2f718491d53f
minkyEazel <minky@eazel.net>
2024-02-04 09:53:36 +0900

    🎨 (base-component): Add flex, grid component for using globally

---
+++ src/components/Widget/Flex/index.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const Flex = styled.div`
+  display: flex;
+  position: relative;
+
+  align-items: start;
+  justify-content: start;
+`
+
+export default Flex
---
+++ src/components/Widget/Grid/index.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+const Grid = styled.div`
+  display: grid;
+  position: relative;
+`
+
+export default Grid

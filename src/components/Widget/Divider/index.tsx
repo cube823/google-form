cbe78425e59c6a9b3e66b18672fe1fa963eee685
minkyEazel <minky@eazel.net>
2024-02-06 12:01:56 +0900

    🎨 (divider): 컴포넌트 추가

---
+++ src/components/Widget/Divider/index.tsx
@@ -0,0 +1,20 @@
+import { styled } from 'styled-components'
+
+interface DividerProps {
+  direction: 'horizontal' | 'vertical'
+  length?: number
+  style?: React.CSSProperties
+}
+
+const Divider = ({ direction, length, style }: DividerProps) => {
+  return <Container direction={direction} length={length} style={style} />
+}
+
+const Container = styled.div<DividerProps>`
+  width: ${({ direction, length }) => (direction === 'vertical' ? '1px' : length ?? '100%')};
+  height: ${({ direction, length }) =>
+    direction === 'vertical' ? `${length}px` ?? '100%' : '1px'};
+  background-color: ${({ theme }) => theme.colors.grey_lighter};
+`
+
+export default Divider

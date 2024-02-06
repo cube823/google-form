b19f7b75fc2d71f6791c32ae848c7d542255b2e0
minkyEazel <minky@eazel.net>
2024-02-06 12:04:13 +0900

    🎨 (main): Theme, Redux Provider 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import store from './store'
@@ -7,18 +6,26 @@ import { Route, BrowserRouter, Routes } from 'react-router-dom'
 import Forms from './pages/Forms'
 import FormResponse from './pages/FormResponse'
 import ViewForm from './pages/ViewForm'
+import { ThemeProvider } from 'styled-components'
+import theme from './style/theme'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
+
+const persistor = persistStore(store)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
     <Provider store={store}>
-      <GlobalStyles />
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Forms />} />
-          <Route path='/viewForm' element={<ViewForm />} />
-          <Route path='/formResponse' element={<FormResponse />} />
-        </Routes>
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <GlobalStyles />
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Forms />} />
+            <Route path='/viewForm' element={<ViewForm />} />
+            <Route path='/formResponse' element={<FormResponse />} />
+          </Routes>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
-  </React.StrictMode>
+  </ThemeProvider>
 )

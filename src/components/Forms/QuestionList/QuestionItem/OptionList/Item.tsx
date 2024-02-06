093ce8a474ee28bf6b771e3aa70fa272defa3d38
minkyEazel <minky@eazel.net>
2024-02-06 11:52:09 +0900

    🎨 (OptionList): QuestionList 의 Option 기능 구현

---
+++ src/components/Forms/QuestionList/QuestionItem/OptionList/Item.tsx
@@ -0,0 +1,86 @@
+import { Draggable } from 'react-beautiful-dnd'
+import {
+  Option,
+  QuestionSlice,
+  removeQuestionOption,
+  updateQuestionOption,
+} from '../../../../../feature/questionSlice'
+import { useAppDispatch } from '../../../../../store'
+import Flex from '../../../../Widget/Flex'
+import Input from '../../../../Widget/Input'
+import Text from '../../../../Widget/Text'
+import Icon from '../../../../Icons'
+
+interface OptionItemProps {
+  index: number
+  optionIndex: number
+  question: QuestionSlice
+  option: Option
+  isCurrent: boolean
+}
+
+const OptionItem = ({ index, optionIndex, isCurrent, question, option }: OptionItemProps) => {
+  const dispatch = useAppDispatch()
+
+  const setValue = (optionIndex: number, val: string) => {
+    dispatch(
+      updateQuestionOption({
+        index,
+        optionIndex,
+        text: val,
+      })
+    )
+  }
+
+  const removeOption = () => dispatch(removeQuestionOption({ index, optionIndex }))
+
+  return (
+    <Draggable index={optionIndex} key={option.id} draggableId={option.id}>
+      {(provided, snapshot) => (
+        <Flex
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          style={{
+            ...provided.draggableProps.style,
+            alignItems: 'center',
+            height: '48px',
+            gap: '6px',
+          }}
+        >
+          {question.questionType === 'dropdown' ? (
+            <Text text={`${optionIndex + 1}`} />
+          ) : (
+            <Icon
+              color='#bdbdbd'
+              iconName={
+                question.questionType === 'checkbox'
+                  ? 'checkbox-outline-blank'
+                  : 'radio-button-unchecked'
+              }
+            />
+          )}
+          <Input
+            value={option.text}
+            onBlur={() => {
+              console.log('blur', option.text)
+              if (option.text === '') {
+                dispatch(
+                  updateQuestionOption({
+                    index,
+                    optionIndex,
+                    text: `옵션 ${optionIndex + 1}`,
+                  })
+                )
+              }
+            }}
+            setValue={(val) => setValue(optionIndex, val)}
+            removeInput={question.options.length > 1 && isCurrent ? removeOption : undefined}
+          />
+        </Flex>
+      )}
+    </Draggable>
+  )
+}
+
+export default OptionItem
---
+++ src/components/Forms/QuestionList/QuestionItem/OptionList/index.tsx
@@ -0,0 +1,73 @@
+import { DragDropContext, Droppable, DropResult } from 'react-beautiful-dnd'
+import { updateQuestionOptionOrder } from '../../../../../feature/questionSlice'
+import { useAppDispatch, useAppSelector } from '../../../../../store'
+import Grid from '../../../../Widget/Grid'
+import Text from '../../../../Widget/Text'
+import AddButton from './AddButton'
+import EtcItem from './EtcItem'
+import OptionItem from './Item'
+
+const QuestionOptionList = ({ index }: { index: number }) => {
+  const dispatch = useAppDispatch()
+  const questions = useAppSelector((state) => state.questionReducer)
+  const { currentIndex } = useAppSelector((state) => state.currentReducer)
+  const question = questions[index]
+
+  const isCurrent = currentIndex === index
+
+  const onDragEnd = ({ destination, source }: DropResult) => {
+    if (!destination) return
+
+    if (source.droppableId === 'droppable') {
+      dispatch(
+        updateQuestionOptionOrder({
+          index,
+          sourceIndex: source.index,
+          destinationIndex: destination.index,
+        })
+      )
+    }
+  }
+
+  const textStyle = {
+    paddingBottom: 8,
+    marginTop: 16,
+    marginBottom: isCurrent ? 24 : 8,
+    width: '50%',
+    borderBottom: '1px solid #e0e0e0',
+  }
+
+  if (question.questionType === 'short')
+    return <Text style={textStyle} color='grey_light' text='단답형 텍스트' />
+
+  if (question.questionType === 'long')
+    return <Text style={{ ...textStyle, width: '75%' }} color='grey_light' text='장문형 텍스트' />
+
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId='droppable' direction='vertical'>
+        {(provided) => (
+          <Grid ref={provided.innerRef} {...provided.droppableProps}>
+            {question.options.map((option, optionIndex) => (
+              <OptionItem
+                key={optionIndex}
+                index={index}
+                optionIndex={optionIndex}
+                option={option}
+                question={question}
+                isCurrent={isCurrent}
+              />
+            ))}
+
+            <EtcItem index={index} isCurrent={isCurrent} question={question} />
+
+            {provided.placeholder}
+          </Grid>
+        )}
+      </Droppable>
+      {isCurrent && <AddButton question={question} index={index} />}
+    </DragDropContext>
+  )
+}
+
+export default QuestionOptionList

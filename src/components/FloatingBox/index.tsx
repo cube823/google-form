4184a126db687888e84ad86cf2943a754b94975b
minkyEazel <minky@eazel.net>
2024-02-06 19:35:16 +0900

    💄 (preview): 미리보기 아이콘 위치 Floating Box 로 이동

--- src/components/FloatingBox/index.tsx
+++ src/components/FloatingBox/index.tsx
@@ -8,6 +8,7 @@ import IconButton from '../Widget/IconButton'
 const FloatingBox = () => {
   const dispatch = useAppDispatch()
   const questions = useAppSelector((state) => state.questionReducer)
+  const navigateToViewForm = () => window.open('/viewForm', '_blank')
 
   const onAddQuestion = () => {
     dispatch(addQuestion({ ...initialQuestionState, id: uuidv4() }))
@@ -15,9 +16,23 @@ const FloatingBox = () => {
   }
 
   return (
-    <Container>
-      <IconButton iconName='add-circle' onClick={onAddQuestion} tooltipText='질문 추가' />
-    </Container>
+    <>
+      <Container>
+        <IconButton
+          side='right'
+          iconName='add-circle'
+          onClick={onAddQuestion}
+          tooltipText='질문 추가'
+        />
+
+        <IconButton
+          side='right'
+          iconName='visibility'
+          onClick={navigateToViewForm}
+          tooltipText='미리보기'
+        />
+      </Container>
+    </>
   )
 }
 
@@ -26,13 +41,15 @@ const Container = styled.div`
   position: sticky;
   justify-content: center;
   align-items: center;
+  gap: 20px;
 
   background-color: ${({ theme }) => theme.colors.white};
   border: 1px solid rgb(218, 220, 224);
   border-radius: 8px;
 
   @media screen and (min-width: 951px) {
-    height: 48px;
+    flex-direction: column;
+    padding: 12px 0;
     width: 48px;
     top: 16px;
 
@@ -45,13 +62,13 @@ const Container = styled.div`
 
   @media screen and (max-width: 950px) {
     position: fixed;
-
     left: 16px;
     bottom: 0;
     height: 60px;
     width: calc(100% - 32px);
     border-radius: 8px 8px 0 0;
     border-width: 1px 1px 0 1px;
+    justify-content: space-around;
 
     box-shadow: 0 2px 1px -1px rgba(0, 0, 0, 0.2), 0 1px 1px 0 rgba(0, 0, 0, 0.14),
       0 1px 3px 0 rgba(0, 0, 0, 0.12);
--- src/components/Forms/index.tsx
+++ src/components/Forms/index.tsx
@@ -4,15 +4,10 @@ import FormHeader from './Header'
 import Flex from '../Widget/Flex'
 import FloatingBox from '../FloatingBox'
 import { DesktopFloatingBox } from './QuestionList/style'
-import IconButton from '../Widget/IconButton'
 
 function Forms() {
-  const navigateToViewForm = () => window.open('/viewForm', '_blank')
-
   return (
     <Layout.Global>
-      <IconButton iconName='visibility' onClick={navigateToViewForm} tooltipText='미리보기' />
-
       <Flex
         style={{
           flexDirection: 'column',

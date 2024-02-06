fd029208f3e5f895d95f25333971c182d82fe6f2
minkyEazel <minky@eazel.net>
2024-02-06 12:03:37 +0900

    🎨 (text): 컴포넌트 추가

---
+++ src/components/Widget/Text/index.tsx
@@ -0,0 +1,54 @@
+import { CSSProperties } from 'react'
+import styled from 'styled-components'
+import { Color } from '../../../style/theme'
+
+interface TextCssProps {
+  fontSize?: number
+  lineHeight?: number
+  color?: Color
+  fontWeight?: number
+
+  style?: CSSProperties
+}
+
+export interface TextBaseProps extends TextCssProps {
+  text: string
+}
+
+const Text = ({
+  text,
+  fontSize = 14,
+  color = 'black',
+  fontWeight = 400,
+  lineHeight,
+  style,
+}: TextBaseProps) => {
+  return (
+    <TextBaseContainer
+      fontSize={fontSize}
+      lineHeight={lineHeight}
+      color={color}
+      fontWeight={fontWeight}
+      style={style}
+    >
+      {text}
+    </TextBaseContainer>
+  )
+}
+
+const TextBaseContainer = styled.div<TextCssProps>`
+  line-height: 100%;
+  font-family: inherit;
+  width: fit-content;
+
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+
+  font-size: ${(props) => props.fontSize}px;
+  font-weight: ${(props) => props.fontWeight};
+  line-height: ${(props) => props.lineHeight}px;
+  color: ${({ theme, color }) => theme.colors[color ?? 'black']};
+`
+
+export default Text

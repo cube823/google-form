eaf70c41fa4b620e748c7eb1c6fffa4b0f92853a
minkyEazel <minky@eazel.net>
2024-02-06 18:52:14 +0900

    🎨 (submit): Submit vailidation 추가

--- src/components/ViewForm/Footer/index.tsx
+++ src/components/ViewForm/Footer/index.tsx
@@ -1,13 +1,46 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import { createPortal } from 'react-dom'
+import { useAppSelector } from '../../../store'
 import Text from '../../Widget/Text'
 import RemoveModal from './RemoveModal'
 import * as S from './style'
 
 const ViewFormFooter = () => {
+  const questions = useAppSelector((state) => state.questionReducer)
   const [removeModalOpen, setRemoveModalOpen] = useState(false)
 
-  const onSubmit = () => window.open('/formResponse', '_blank')
+  const validateRequired = useCallback(() => {
+    const requiredList = questions.filter((question) => question.isRequired)
+    let isAllAnswered = true
+
+    if (requiredList.length === 0) return true
+
+    requiredList.forEach((question) => {
+      if (question.questionType === 'short' || question.questionType === 'long') {
+        if (!question.answerText) {
+          console.log('answerText', question.answerText)
+          isAllAnswered = false
+        }
+      }
+
+      if (
+        question.questionType !== 'short' &&
+        question.questionType !== 'long' &&
+        !question.options.some((option) => option.answerChecked)
+      ) {
+        if (!question.etcOption.checked) {
+          isAllAnswered = false
+        }
+      }
+    })
+
+    return isAllAnswered
+  }, [questions])
+
+  const onSubmit = () => {
+    if (!validateRequired()) return alert('필수 질문을 모두 작성해주세요.')
+    window.open('/formResponse', '_blank')
+  }
 
   const onRemoveModalOpen = () => setRemoveModalOpen(true)
   const onRemoveModalClose = () => {

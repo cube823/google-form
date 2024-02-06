216412f9dd1810bee2847c5ac88262801eefdd87
minkyEazel <minky@eazel.net>
2024-02-06 12:01:10 +0900

    💄 (IconButton): UI 미세변경

--- src/components/Widget/IconButton/index.tsx
+++ src/components/Widget/IconButton/index.tsx
@@ -1,20 +1,37 @@
+import { CSSProperties } from 'react'
 import { styled } from 'styled-components'
 import Icon, { IconSymbol } from '../../Icons'
 
-const IconButton = ({ iconName, onClick }: { iconName: IconSymbol; onClick: () => void }) => {
-  return <IContainer color='#5f6368' iconName={iconName} onClick={onClick} />
+interface IconButtonProps {
+  iconName: IconSymbol
+  onClick: () => void
+  style?: CSSProperties
+}
+
+const IconButton = ({ iconName, onClick, style }: IconButtonProps) => {
+  return <IContainer color='#5f6368' style={style} iconName={iconName} onClick={onClick} />
 }
 
 const IContainer = styled(Icon)`
-  height: 48px;
-  width: 48px;
+  height: 24px;
+  width: 24px;
   display: flex;
   align-items: center;
   justify-content: center;
+  position: relative;
 
   &:hover {
-    background-color: rgba(0, 0, 0, 0.04);
-    border-radius: 50%;
+    &::before {
+      content: '';
+      position: absolute;
+      top: -8px;
+      left: -8px;
+      width: calc(100% + 16px);
+      height: calc(100% + 16px);
+
+      background-color: rgba(0, 0, 0, 0.04);
+      border-radius: 50%;
+    }
   }
 `
 

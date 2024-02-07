adb7c50c637757c60023a3b8cd948ba80048f66a
minkyEazel <minky@eazel.net>
2024-02-08 00:09:19 +0900

    🎨 (option-item): Hover 시 drag button 및 border-bottom 활성화 되도록 변경

--- src/components/Forms/QuestionList/QuestionItem/OptionList/Item.tsx
+++ src/components/Forms/QuestionList/QuestionItem/OptionList/Item.tsx
@@ -10,6 +10,7 @@ import Flex from '../../../../Widget/Flex'
 import Input from '../../../../Widget/Input'
 import Text from '../../../../Widget/Text'
 import Icon from '../../../../Icons'
+import { memo, useCallback, useState } from 'react'
 
 interface OptionItemProps {
   index: number
@@ -21,18 +22,25 @@ interface OptionItemProps {
 
 const OptionItem = ({ index, optionIndex, isCurrent, question, option }: OptionItemProps) => {
   const dispatch = useAppDispatch()
+  const [isHovered, setIsHovered] = useState(false)
 
-  const setValue = (optionIndex: number, val: string) => {
-    dispatch(
-      updateQuestionOption({
-        index,
-        optionIndex,
-        text: val,
-      })
-    )
-  }
+  const setValue = useCallback(
+    (optionIndex: number, val: string) => {
+      dispatch(
+        updateQuestionOption({
+          index,
+          optionIndex,
+          text: val,
+        })
+      )
+    },
+    [dispatch, index]
+  )
 
-  const removeOption = () => dispatch(removeQuestionOption({ index, optionIndex }))
+  const removeOption = useCallback(
+    () => dispatch(removeQuestionOption({ index, optionIndex })),
+    [dispatch, index, optionIndex]
+  )
 
   return (
     <Draggable index={optionIndex} key={option.id} draggableId={option.id}>
@@ -40,6 +48,8 @@ const OptionItem = ({ index, optionIndex, isCurrent, question, option }: OptionI
         <Flex
           ref={provided.innerRef}
           {...provided.draggableProps}
+          onMouseOver={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
           style={{
             ...provided.draggableProps.style,
             alignItems: 'center',
@@ -49,11 +59,14 @@ const OptionItem = ({ index, optionIndex, isCurrent, question, option }: OptionI
             boxShadow: snapshot.isDragging ? '0px 4px 4px rgba(0, 0, 0, 0.25)' : 'none',
           }}
         >
-          <Icon
-            {...provided.dragHandleProps}
-            iconName='drag-indicator'
-            style={{ position: 'absolute', left: -24 }}
-          />
+          {isHovered && (
+            <Icon
+              {...provided.dragHandleProps}
+              iconName='drag-indicator'
+              style={{ position: 'absolute', left: -18, width: 20, height: 20 }}
+              color='gray_lighter'
+            />
+          )}
           {question.questionType === 'dropdown' ? (
             <Text text={`${optionIndex + 1}`} />
           ) : (
@@ -82,6 +95,7 @@ const OptionItem = ({ index, optionIndex, isCurrent, question, option }: OptionI
             setValue={(val) => setValue(optionIndex, val)}
             removeInput={question.options.length > 1 && isCurrent ? removeOption : undefined}
             hoverDisabled={!isCurrent}
+            hasBorderBottom={isCurrent && isHovered}
           />
         </Flex>
       )}
@@ -89,4 +103,4 @@ const OptionItem = ({ index, optionIndex, isCurrent, question, option }: OptionI
   )
 }
 
-export default OptionItem
+export default memo(OptionItem)
